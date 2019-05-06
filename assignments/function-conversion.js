dd4a3abce2b322e00cf885f7e535abb14c81fbc7
kmcknight1 <kmcknight1991@gmail.com>
2019-05-06 16:50:56 -0500

    MVP and Stretch complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,44 @@
 // };
 // myFunction();
 
+const myFunction = () => {
+  return "Function was invoked!";
+};
+
+console.log(myFunction());
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = param => {
+  return param;
+};
+
+console.log(anotherFunction("Exapmle"));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (x, y) => {
+  return x + y;
+};
+
+console.log(add(1, 2));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+const subtract = (x, y) => {
+  return x - y;
+};
+
+console.log(subtract(1, 2));
 
 // Stretch
 
@@ -28,3 +51,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+exampleArray = [1, 2, 3, 4];
+
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
